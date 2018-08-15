fb0834f07a37da51f5d6ff03ead0a56bdfbfd7d7
liwei272 <liwei272@chinaunicom.cn>
2018-08-15 11:30:15 +0800

    OK

--- src/main/java/com/unicom/clgl/App.java
+++ src/main/java/com/unicom/clgl/App.java
@@ -3,7 +3,7 @@ package com.unicom.clgl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//启动类
+//启动类 
 @SpringBootApplication
 public class App {
 
